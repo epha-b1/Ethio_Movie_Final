efa5d8dffd98898ae614690e26bee96738c50cb7
Ephrem B <ephremyonatan27@gmail.com>
2024-05-12 20:06:59 +0300

    "Update NewSerious.jsx: add seasons and episodes, remove unnecessary file inputs, use axios for API call"

--- admin/src/pages/newSerious/NewSerious.jsx
+++ admin/src/pages/newSerious/NewSerious.jsx
@@ -1,39 +1,22 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import "./newSerious.css";
 import { storage } from "../../firebase";
 import { ref, getDownloadURL, uploadBytesResumable } from "@firebase/storage";
 import { createSerious } from "../../context/seriesContext/apiCalls";
 import { SeriousContext } from "../../context/seriesContext/SeriousContext";
 import CircularProgress from "@mui/material/CircularProgress";
+import axios from "axios";
 
 export default function NewSerious() {
-  const [Series, setSeries] = useState({
+  const [series, setSeries] = useState({
     title: "",
     description: "",
-    img: null,
-    imgTitle: null,
-    imgSm: null,
-    trailer: null,
-    video: null,
-    year: "",
-    genre: "",
+    thumbnail: "",
+    rating: 0,
+    genre: [],
     language: "",
     country: "",
-    seasons: [
-      {
-        seasonNumber: 1,
-        episodes: [
-          {
-            episodeNumber: 1,
-            title: "",
-            description: "",
-            duration: "",
-            thumbnail: null,
-            url: "",
-          },
-        ],
-      },
-    ],
+    seasons: [],
   });
 
   const [uploaded, setUploaded] = useState(0);
@@ -42,57 +25,101 @@ export default function NewSerious() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setSeries({ ...Series, [name]: value });
+    setSeries({ ...series, [name]: value });
   };
 
-  const handleFileChange = (e, label) => {
+  const handleFileChange = (e, label, seasonIndex, episodeIndex) => {
     const file = e.target.files[0];
-    setSeries({ ...Series, [label]: file });
-  };
-
-  const upload = (items) => {
-    items.forEach((item) => {
-      const fileName = new Date().getTime() + item.label + item.file.name;
-      const storageRef = ref(storage, `tvSeries/${fileName}`);
-      const uploadTask = uploadBytesResumable(storageRef, item.file);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setUploadProgress(progress);
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-            setSeries((prev) => {
-              return { ...prev, [item.label]: url };
-            });
-            setUploaded((prev) => prev + 1);
-          });
-        }
-      );
+    setSeries((prevSeries) => {
+      const updatedSeasons = [...prevSeries.seasons];
+      updatedSeasons[seasonIndex].episodes[episodeIndex][label] = file;
+      return { ...prevSeries, seasons: updatedSeasons };
     });
   };
 
-  const handleUpload = (e) => {
-    e.preventDefault();
-    upload([
-      { file: Series.img, label: "img" },
-      { file: Series.imgTitle, label: "imgTitle" },
-      { file: Series.imgSm, label: "imgSm" },
-      { file: Series.trailer, label: "trailer" },
-      { file: Series.video, label: "video" },
-    ]);
+  const addSeason = () => {
+    setSeries((prevSeries) => ({
+      ...prevSeries,
+      seasons: [
+        ...prevSeries.seasons,
+        {
+          seasonNumber: prevSeries.seasons.length + 1,
+          episodes: [],
+        },
+      ],
+    }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createSerious(Series, dispatch);
+  const addEpisode = (seasonIndex) => {
+    setSeries((prevSeries) => {
+      const updatedSeasons = [...prevSeries.seasons];
+      const newEpisode = {
+        episodeNumber: updatedSeasons[seasonIndex].episodes.length + 1,
+        title: "",
+        description: "",
+        duration: "",
+        thumbnail: null,
+        url: "",
+      };
+      updatedSeasons[seasonIndex] = {
+        ...updatedSeasons[seasonIndex],
+        episodes: [...updatedSeasons[seasonIndex].episodes, newEpisode],
+      };
+      return { ...prevSeries, seasons: updatedSeasons };
+    });
   };
+
+  const upload = (file, label, seasonIndex, episodeIndex) => {
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, `tvSeries/${fileName}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setSeries((prev) => {
+            const updatedSeasons = [...prev.seasons];
+            updatedSeasons[seasonIndex].episodes[episodeIndex][label] = url;
+            return { ...prev, seasons: updatedSeasons };
+          });
+          setUploaded((prev) => prev + 1);
+        });
+      }
+    );
+  };
+
+  const handleUpload = (e, label, seasonIndex, episodeIndex) => {
+    e.preventDefault();
+    const file = series.seasons[seasonIndex].episodes[episodeIndex][label];
+    upload(file, label, seasonIndex, episodeIndex);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      try {
+        const res = await axios.post("http://localhost:8800/api/serious",series, {
+          headers: {
+            token:
+              "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+          },
+        });
+
+        
+      } catch (error) {
+        console.error("Error serious adding:", error);
+      }
+    
+  };
+  
+  
 
   return (
     <div className="newProduct">
@@ -104,7 +131,7 @@ export default function NewSerious() {
             type="text"
             placeholder="Title"
             name="title"
-            value={Series.title}
+            value={series.title}
             onChange={handleChange}
           />
         </div>
@@ -114,17 +141,25 @@ export default function NewSerious() {
             type="text"
             placeholder="Description"
             name="description"
-            value={Series.description}
+            value={series.description}
             onChange={handleChange}
           />
         </div>
-        <div className="addProductItem">
-          <label>Year</label>
+        {/* <div className="addProductItem">
+          <label>Thumbnail</label>
           <input
-            type="text"
-            placeholder="Year"
-            name="year"
-            value={Series.year}
+            type="file"
+            name="thumbnail"
+            onChange={(e) => handleFileChange(e, "thumbnail")}
+          />
+        </div> */}
+        <div className="addProductItem">
+          <label>Rating</label>
+          <input
+            type="number"
+            placeholder="Rating"
+            name="rating"
+            value={series.rating}
             onChange={handleChange}
           />
         </div>
@@ -134,8 +169,8 @@ export default function NewSerious() {
             type="text"
             placeholder="Genre"
             name="genre"
-            value={Series.genre}
-            onChange={handleChange}
+            value={series.genre.join(", ")}
+            onChange={(e) => setSeries({ ...series, genre: e.target.value.split(", ") })}
           />
         </div>
         <div className="addProductItem">
@@ -144,7 +179,7 @@ export default function NewSerious() {
             type="text"
             placeholder="Language"
             name="language"
-            value={Series.language}
+            value={series.language}
             onChange={handleChange}
           />
         </div>
@@ -154,59 +189,104 @@ export default function NewSerious() {
             type="text"
             placeholder="Country"
             name="country"
-            value={Series.country}
+            value={series.country}
             onChange={handleChange}
           />
         </div>
-        <div className="addProductItem">
-          <label>Image</label>
-          <input
-            type="file"
-            name="img"
-            onChange={(e) => handleFileChange(e, "img")}
-          />
-        </div>
-        <div className="addProductItem">
-          <label>Title Image</label>
-          <input
-            type="file"
-            name="imgTitle"
-            onChange={(e) => handleFileChange(e, "imgTitle")}
-          />
-        </div>
-        <div className="addProductItem">
-          <label>Thumbnail Image</label>
-          <input
-            type="file"
-            name="imgSm"
-            onChange={(e) => handleFileChange(e, "imgSm")}
-          />
-        </div>
-        <div className="addProductItem">
-          <label>Trailer</label>
-          <input
-            type="file"
-            name="trailer"
-            onChange={(e) => handleFileChange(e, "trailer")}
-          />
-        </div>
-        <div className="addProductItem">
-          <label>Video</label>
-          <input
-            type="file"
-            name="video"
-            onChange={(e) => handleFileChange(e, "video")}
-          />
-        </div>
-        {uploaded === 5 ? (
-          <button className="addProductButton" onClick={handleSubmit}>
-            Create
-          </button>
-        ) : (
-          <button className="addProductButton" onClick={handleUpload}>
-            Upload
-          </button>
-        )}
+        {series.seasons.map((season, seasonIndex) => (
+          <div key={season._id}>
+            <h2>Season {season.seasonNumber}</h2>
+            <button type="button" onClick={() => addEpisode(seasonIndex)}>Add Episode</button>
+            {season.episodes.map((episode, episodeIndex) => (
+              <div key={episode._id}>
+                <div className="addProductItem">
+                  <label>Title</label>
+                  <input
+                    type="text"
+                    placeholder="Title"
+                    value={episode.title}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      setSeries((prevSeries) => {
+                        const updatedSeasons = [...prevSeries.seasons];
+                        updatedSeasons[seasonIndex].episodes[episodeIndex].title = value;
+                        return { ...prevSeries, seasons: updatedSeasons };
+                      });
+                    }}
+                  />
+                </div>
+                <div className="addProductItem">
+                  <label>Description</label>
+                  <input
+                    type="text"
+                    placeholder="Description"
+                    value={episode.description}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      setSeries((prevSeries) => {
+                        const updatedSeasons = [...prevSeries.seasons];
+                        updatedSeasons[seasonIndex].episodes[episodeIndex].description = value;
+                        return { ...prevSeries, seasons: updatedSeasons };
+                      });
+                    }}
+                  />
+                </div>
+                <div className="addProductItem">
+                  <label>Duration</label>
+                  <input
+                    type="text"
+                    placeholder="Duration"
+                    value={episode.duration}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      setSeries((prevSeries) => {
+                        const updatedSeasons = [...prevSeries.seasons];
+                        updatedSeasons[seasonIndex].episodes[episodeIndex].duration = value;
+                        return { ...prevSeries, seasons: updatedSeasons };
+                      });
+                    }}
+                  />
+                </div>
+                <div className="addProductItem">
+                  <label>Thumbnail</label>
+                  <input
+                    type="file"
+                    onChange={(e) => handleFileChange(e, "thumbnail", seasonIndex, episodeIndex)}
+                  />
+                  {episode.thumbnail && (
+                    <img src={episode.thumbnail} alt="Episode Thumbnail" />
+                  )}
+                  <button
+                    className="addProductButton"
+                    onClick={(e) => handleUpload(e, "thumbnail", seasonIndex, episodeIndex)}
+                  >
+                    Upload Thumbnail
+                  </button>
+                </div>
+                <div className="addProductItem">
+                  <label>URL</label>
+                  <input
+                    type="file"
+                    onChange={(e) => handleFileChange(e, "url", seasonIndex, episodeIndex)}
+                  />
+                  {episode.url && (
+                    <a href={episode.url}>Episode URL</a>
+                  )}
+                  <button
+                    className="addProductButton"
+                    onClick={(e) => handleUpload(e, "url", seasonIndex, episodeIndex)}
+                  >
+                    Upload URL
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+        <button type="button" onClick={addSeason}>Add Season</button>
+       
+          <button className="addProductButton" onClick={handleSubmit}>Create</button>
+        
       </form>
       {uploaded < 5 && (
         <div className="progressIndicator">
