221bafa13e40b739be42b80ffa0ce815c4ad8abf
Ephrem B <ephremyonatan27@gmail.com>
2024-05-15 00:03:30 +0300

    "Update movie view count on watch page load"
    This commit updates the view count of a movie in the database each time the watch page for that movie is loaded. This is done using the `useEffect` hook in React, which runs

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,10 +1,23 @@
-import { ArrowBackOutlined } from "@material-ui/icons";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { Link, useLocation } from "react-router-dom";
+import { ArrowBackOutlined } from "@material-ui/icons";
 import "./watch.scss";
 
 export default function Watch() {
   const location = useLocation();
   const movie = location.movie;
+  useEffect(() => {
+    const updateViews = async () => {
+      try {
+        await axios.post(`/movies/${movie._id}/views`);
+      } catch (error) {
+        console.error("Error updating views:", error);
+      }
+    };
+    updateViews();
+  }, [movie]);
+
   return (
     <div className="watch">
       <Link to="/">
