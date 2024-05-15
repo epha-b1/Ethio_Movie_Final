b0862d87ea4b007206b3765497258a5415429725
Ephrem B <ephremyonatan27@gmail.com>
2024-05-15 14:41:32 +0300

    "Add user mapping for movie and series uploaders in WidgetLg component"
    This commit updates the WidgetLg component to display the username of the user who uploaded each movie and series. It fetches user data based on the user

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 export default function WidgetLg() {
   const { movies, dispatch: movieDispatch } = useContext(MovieContext);
   const [series, setSeries] = useState([]);
+  const [userMap, setUserMap] = useState({});
 
   useEffect(() => {
     getMovies(movieDispatch);
@@ -31,6 +32,47 @@ export default function WidgetLg() {
     fetchSeries();
   }, []);
 
+  const fetchUserById = async (userId) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8800/api/users/find/${userId}`
+      );
+
+      // Check if the response contains the user object and the username field
+      if (response.data && response.data.user && response.data.user.username) {
+        return response.data;
+      } else {
+        console.error("Username not found in the response:", response.data);
+        return { user: { username: "Unknown" }, isSubscribed: false }; // Return a default value if username is not found
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      return { user: { username: "Unknown" }, isSubscribed: false }; // Return a default value in case of an error
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userPromises = movies.map(async (movie) => {
+        const userData = await fetchUserById(movie.uploadedBy);
+        return { userId: movie.uploadedBy, username: userData.user.username ,role:userData.user.username || "Unknown" };
+      });
+
+      const userResults = await Promise.all(userPromises);
+
+      const userMap = {};
+      userResults.forEach(user => {
+        userMap[user.userId] = user.username;
+        userMap[user.userId] = user.role;
+      });
+
+      setUserMap(userMap);
+    };
+
+    fetchData();
+
+  }, [movies]);
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest Upload</h3>
@@ -42,11 +84,11 @@ export default function WidgetLg() {
             <th className="widgetLgTh">Views</th>
             <th className="widgetLgTh">UploadedBy</th>
           </tr>
-          {movies.map((movie) => (
+          {movies.map(movie => (
             <tr className="widgetLgTr" key={movie._id}>
               <td className="widgetLgUser">
                 <img
-                  src={movie.thumbnail} // Assuming `thumbnail` is the property for movie thumbnail
+                  src={movie.thumbnail}
                   alt=""
                   className="widgetLgImg"
                 />
@@ -54,14 +96,14 @@ export default function WidgetLg() {
               </td>
               <td className="widgetLgGenre">{movie.genre}</td>
               <td className="widgetLgStatus">{movie.views}</td>
-              <td className="widgetLgStatus">{movie.uploadedBy}</td>
+              <td className="widgetLgStatus">{userMap[movie.uploadedBy] || "Loading..."}</td>
             </tr>
           ))}
-          {series.map((serie) => (
+          {series.map(serie => (
             <tr className="widgetLgTr" key={serie._id}>
               <td className="widgetLgUser">
                 <img
-                  src={serie.seasons[0].episodes[0].thumbnail} // Assuming `thumbnail` is the property for series thumbnail
+                  src={serie.seasons[0].episodes[0].thumbnail}
                   alt=""
                   className="widgetLgImg"
                 />
@@ -69,7 +111,7 @@ export default function WidgetLg() {
               </td>
               <td className="widgetLgGenre">{serie.genre}</td>
               <td className="widgetLgStatus">{serie.views}</td>
-              <td className="widgetLgStatus">{serie.uploadedBy}</td>
+              <td className="widgetLgStatus">{userMap[serie.uploadedBy] || "Loading..."}</td>
             </tr>
           ))}
         </tbody>
