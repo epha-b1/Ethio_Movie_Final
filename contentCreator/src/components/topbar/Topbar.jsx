c690e15f06e4405d4064fdce155d642c88f36f1c
Ephrem B <ephremyonatan27@gmail.com>
2024-05-16 06:49:25 +0300

    "Update logo text in Topbar component from 'EthioView Admin' to 'EthioView Content Creator'"

--- contentCreator/src/components/topbar/Topbar.jsx
+++ contentCreator/src/components/topbar/Topbar.jsx
@@ -20,7 +20,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">EthioView Admin</span>
+          <span className="logo">EthioView Content Creator</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
