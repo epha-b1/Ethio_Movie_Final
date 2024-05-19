e64d4bbef5f2ea3605026f90f8d980aa9ba72d46
Ephrem B <ephremyonatan27@gmail.com>
2024-05-19 10:31:13 +0300

    "Updated login API call to handle email/phone number input, added role check after login, and modified error handling"

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -1,32 +1,47 @@
-import axios from "axios"; // Import axios for making HTTP requests
+import axios from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 import { toast } from "sonner";
 
 export const login = async (user, dispatch) => {
-  const { credential, password } = user; // Rename 'email' to 'credential' to accept either email or phone number
-  // Check if credential or password is empty
+  const { credential, password } = user;
+
   if (!credential.trim() || !password) {
     toast.error("Please enter both email/phone number and password.");
     return;
   }
-  // Check if the password is greater than 6 characters
+
   if (password.length < 6) {
     toast.error("Password must be greater than 6 characters.");
     return;
   }
 
   dispatch(loginStart());
+
   try {
-    // Determine if the credential is an email or phone number
-    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(credential); // Check if the credential matches email format
+    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(credential);
+
+    // Send login request based on credential type
     const res = await axios.post("auth/login", {
       [isEmail ? "email" : "phoneNumber"]: credential,
       password,
     });
-    dispatch(loginSuccess(res.data));
-    toast.success("Login successful!"); // Display success toast
+
+    // Retrieve user role name
+    const roleRes = await axios.get(
+      `http://localhost:8800/api/role/${res.data.role}`
+    );
+    const roleName = roleRes.data.role_name;
+
+    // Dispatch login success action based on role
+    if (roleName === "User") {
+      dispatch(loginSuccess(res.data));
+      toast.success("Login successful!");
+    } else {
+      dispatch(loginFailure());
+      toast.error("You are not an User. Please log in with an User account.");
+        }
   } catch (err) {
     dispatch(loginFailure());
-    toast.error("Invalid email/phone number or password. Please try again."); // Display error toast
+    toast.error("Invalid email/phone number or password. Please try again.");
   }
 };
