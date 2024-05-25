b33ab8494d3b3be3a5a7f69c44b2571e7f32a9b7
Ephrem B <ephremyonatan27@gmail.com>
2024-05-26 02:04:27 +0300

    "Added onClick event handler to sidebar list items to toggle active class"

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -18,71 +18,78 @@ import {
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
+  const handleItemClick = (event) => {
+    const activeItem = document.querySelector('.sidebarListItem.active');
+    if (activeItem) {
+      activeItem.classList.remove('active');
+    }
+    event.currentTarget.classList.add('active');
+  };
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <Link to="/" className="link">
-              <li className="sidebarListItem active">
+            <Link to="/" className="link" >
+              <li className="sidebarListItem active" onClick={handleItemClick}>
                 <LineStyle className="sidebarIcon" />
                 Home
               </li>
             </Link>
-            <li className="sidebarListItem">
+            <li className="sidebarListItem"onClick={handleItemClick}>
               <Timeline className="sidebarIcon" />
               <Link to="/analytics" className="link">
-
-Analytics
-</Link>            </li>
-            <li className="sidebarListItem">
+                Analytics
+              </Link>{" "}
+            </li>
+            {/* <li className="sidebarListItem">
               <TrendingUp className="sidebarIcon" />
               Sales
-            </li>
+            </li> */}
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
             <Link to="/users" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <PermIdentity className="sidebarIcon" />
                 Users
               </li>
             </Link>
             <Link to="/movies" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <PlayCircleOutline className="sidebarIcon" />
                 Movies
               </li>
             </Link>
             <Link to="/series" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <PlayArrow className="sidebarIcon" />
                 Series
               </li>
             </Link>
-            <Link to="/lists" className="link">
-              <li className="sidebarListItem">
+            <Link to="/lists" className="link" >
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <List className="sidebarIcon" />
                 Lists
               </li>
             </Link>
             <Link to="/newMovie" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <AddToQueue className="sidebarIcon" />
                 Add Movie
               </li>
             </Link>
             <Link to="/newSerious" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <AddToQueue className="sidebarIcon" />
                 Add Serious
               </li>
             </Link>
             <Link to="/newList" className="link">
-              <li className="sidebarListItem">
+              <li className="sidebarListItem" onClick={handleItemClick}>
                 <QueuePlayNext className="sidebarIcon" />
                 Add List
               </li>
@@ -92,15 +99,15 @@ Analytics
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Notifications</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
+            <li className="sidebarListItem"  onClick={handleItemClick}>
               <MailOutline className="sidebarIcon" />
               Mail
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebarListItem" onClick={handleItemClick}>
               <DynamicFeed className="sidebarIcon" />
               Feedback
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebarListItem" onClick={handleItemClick}>
               <ChatBubbleOutline className="sidebarIcon" />
               Messages
             </li>
@@ -109,15 +116,15 @@ Analytics
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Staff</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
+            <li className="sidebarListItem" onClick={handleItemClick}>
               <WorkOutline className="sidebarIcon" />
               Manage
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebarListItem" onClick={handleItemClick}>
               <Timeline className="sidebarIcon" />
               Analytics
             </li>
-            <li className="sidebarListItem">
+            <li className="sidebarListItem" onClick={handleItemClick}>
               <Report className="sidebarIcon" />
               Reports
             </li>
