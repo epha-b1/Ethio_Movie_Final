22a8d7535022228b1d64161c47779eee9282fc0d
Ephrem B <ephremyonatan27@gmail.com>
2024-05-25 13:15:20 +0300

    "Added Swagger UI to API, updated Swagger configuration and moved setup to index.js"

--- api/swagger.js
+++ api/swagger.js
@@ -1,32 +1,18 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: '3.0.0', // Specify OpenAPI version
     info: {
-      title: 'My API',
-      version: '1.0.0',
-      description: 'A simple API',
-      contact: {
-        email: 'your_email@example.com'
-      },
-      license: {
-        name: 'MIT',
-        url: 'http://opensource.org/licenses/MIT'
-      }
+      title: 'Your API', // Title of the API
+      version: '1.0.0', // Version of the API
+      description: 'API documentation', // Description of the API
     },
-    servers: [
-      {
-        url: 'http://localhost:8800',
-      },
-    ],
   },
-  apis: ['./routes/*.js'],
+  // Path to the API files
+  apis: ['./models/**/*.js', './routes/**/*.js'],
 };
 
-const specs = swaggerJSDoc(options);
+const specs = swaggerJsdoc(options);
 
-module.exports = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-};
+module.exports = specs;
